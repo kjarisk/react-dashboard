d9d2975c07194a1d8769ab8f1fdfcdee7c020484
Kjartan <kjarisk@gmail.com>
2019-08-20 21:42:12 +0200

    Added styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import Welcome from './WelcomeMessage';
 import './App.css';
+
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`;
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Welcome />
+      <MyButton primary>Hello</MyButton>
     </div>
   );
 }
